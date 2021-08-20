c9a0526af56cb6391b3fc3de8f9bdbdcbead79e2
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-08-21 02:12:51 +0300

    Initial Commit

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Никита on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
